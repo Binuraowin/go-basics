59f5f15c910e195dcad18e95403ad25958f69e37
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2023-01-06 23:24:48 +0530

    print statement

---
+++ tutorial.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "hello world"
+	age := 4
+	zero := "hey"
+	fmt.Printf(zero)
+	fmt.Println(age)
+	fmt.Println(name)
+	fmt.Printf("%b", age)
+}
